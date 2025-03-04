22d9e980dfa064154f6e1209366e6f530527e9ff
litongtong <18831013829@163.com>
2025-03-04 13:32:06 +0800

    移动端适配

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,18 @@
 <van-button type="default">默认按钮</van-button>
 <van-button type="danger">危险按钮</van-button>
 <van-button type="warning">警告按钮</van-button>
+<div class="box">
 
+</div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-
+.box
+{
+  width: 150px;
+  height: 100px;
+  background-color: red;
+}
 </style>
