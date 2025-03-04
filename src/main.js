e9754a63de99141ac1e078b9b6a00e62f2a38a67
litongtong <18831013829@163.com>
2025-03-04 13:22:35 +0800

    初始化 使用yarn和vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/utils/vant-ui'
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
 
+// Vue.use(Vant)
+// import { Button } from 'vant'
+// Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
