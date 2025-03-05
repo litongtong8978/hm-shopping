6fe700b77cb270d64421f22e18cff68cc9a8a3a2
litongtong <18831013829@163.com>
2025-03-05 15:18:18 +0800

    手机号图片短信验证

--- src/api/login.js
+++ src/api/login.js
@@ -2,3 +2,12 @@ import request from '@/utils/request'
 export const getPicCode = () => {
   return request.get('/captcha/image')
 }
+export const getMsgCode = (captchaCode, captchaKey, mobile) => {
+  return request.post('/captcha/sendSmsCaptcha', {
+    form: {
+      captchaCode,
+      captchaKey,
+      mobile
+    }
+  })
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -19,7 +19,7 @@
         <div class="form-item">
           <input v-model="msgCode" class="inp" placeholder="请输入短信验证码" type="text">
           <button @click="getCode">
-            {{ second === totalSecond ? '获取验证码' : second + '秒后重新发送'}}
+        {{ second === totalSecond?'获取验证码':`${second}秒后重新发送`}}
           </button>
         </div>
       </div>
@@ -30,7 +30,7 @@
 </template>
 
 <script lang="ts">
-import { getPicCode } from '@/api/login'
+import { getPicCode, getMsgCode } from '@/api/login'
 // import { Toast } from 'vant'// 挂载到Vue原型上，方便全局调用,不需要每个页面都引入 ,这里是按需引入,只引入Toast
 export default {
   name: 'LoginIndex',
@@ -40,7 +40,9 @@ export default {
       picKey: '',
       picUrl: '',
       totalSecond: 60,
-      second: 60
+      second: 60,
+      timer: null,
+      mobile: ''
     }
   },
   async created () {
@@ -50,18 +52,43 @@ export default {
     async getPicCode () {
       const res = await getPicCode()
       this.picKey = res.data.key
-      this.picUrl = res.data.url
-      this.$toast.success('成功')// 方法挂载到Vue原型上，方便全局调用,不需要每个页面都引入
+      // this.picUrl = res.data.url
+      // this.$toast.success('成功')// 方法挂载到Vue原型上，方便全局调用,不需要每个页面都引入
     },
-    getCode () {
-      console.log('获取验证码')
-      setInterval(() => {
+    async getCode () {
+      // console.log('获取验证码')
+      if (!this.vaildFn()) {
+        return
+      }
+      // 验证码和手机号均正确
+      await getMsgCode(this.getPicCode, this.PublicKeyCredential, this.mobile)
+      this.$Toast('短信发送成功')
+      this.timer = setInterval(() => {
         this.second--
         if (this.second <= 0) {
+          clearInterval(this.timer)
+          this.timer = null// 顺序不能换
           this.second = this.totalSecond
         }
       }, 1000)
+    },
+    login () {
+      console.log('j')
+    },
+    vaildFn () {
+      if (!/^1[3-9]\d{9}$/.test(this.mobile)) {
+        this.$toast('请输入正确手机号')
+        return false
+      }
+      if (/^\w{4}$/.test(this.picCode)) {
+        this.$toast('请输入正确验证码')
+        return false
+      }
+      return true
     }
+  },
+  destroyed () {
+    clearInterval(this.timer)// 离开页面销毁定时器
   }
 
 }
